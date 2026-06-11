e1a2beb21f1a7c4d430891a49e0337c975195b86
jonty1231 <jontythakur68@gmail.com>
2026-06-11 21:49:33 +0530

    Ad

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -72,7 +72,7 @@ const HeroSlider = () => {
         `}</style> */}
 
          <img 
-                src={`/banner/desktop/SAVE_20260611_125832.jpeg`} 
+                src={`/banner/desktop/9.wepb`} 
                 alt={`Hero Banner `}
                 
                 priority 
@@ -84,7 +84,7 @@ const HeroSlider = () => {
       <Link href={'/products'} className="relative w-full h-[750px] overflow-hidden bg-neutral-950 md:hidden">
 
         <img 
-                src={`/banner/desktop/SAVE_20260611_125832.jpeg`} 
+                src={`/banner/desktop/9.wepb`} 
                 alt={`Hero Banner `}
                 
                 priority 
